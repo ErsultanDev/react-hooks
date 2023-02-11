283af7667d28b0bad99fad1c8301ffd16ac95589
Ersultan <example@gmail.com>
2023-02-11 18:38:18 +0300

    comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import Routes from 'routes';
-import TopBar from 'components/topbar';
-import { CurrentUserProvider } from 'contexts/currentUser';
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "routes";
+import TopBar from "components/topbar";
+import { CurrentUserProvider } from "contexts/currentUser";
 
-import CurrentUserChecker from 'components/currentUserChecker';
+import CurrentUserChecker from "components/currentUserChecker";
 
 function App() {
-	return (
-		<div className="App">
-			<CurrentUserProvider>
-				<CurrentUserChecker>
-					<Router>
-						<TopBar />
-						<Routes />
-					</Router>
-				</CurrentUserChecker>
-			</CurrentUserProvider>
-		</div>
-	);
+  return (
+    <div className="App">
+      <CurrentUserProvider>
+        <CurrentUserChecker>
+          <Router>
+            <TopBar />
+            <Routes />
+          </Router>
+        </CurrentUserChecker>
+      </CurrentUserProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Routes from 'routes'
+import Topbar from 'components/topbar'
+import {CurrentUserProvider} from 'contexts/currentUser'
+import CurrentUserChecker from 'components/currentUserChecker'
+
+const App = () => {
+  return (
+    <CurrentUserProvider>
+      <CurrentUserChecker>
+        <Router>
+          <Topbar />
+          <Routes />
+        </Router>
+      </CurrentUserChecker>
+    </CurrentUserProvider>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
