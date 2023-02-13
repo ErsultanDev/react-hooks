017871192aaf00c4c69a9aba6cd91cb1f38123f8
Ersultan <example@gmail.com>
2023-02-13 18:49:32 +0300

    Контекст текущего юзера

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Routes from 'routes'
 import TopBar from 'components/topbar'
-// import Routes from 'routes' import Topbar from 'components/topbar' import
-// {CurrentUserProvider} from 'contexts/currentUser' import CurrentUserChecker
-// from 'components/currentUserChecker'
+// import Routes from 'routes' import Topbar from 'components/topbar'
+import {CurrentUserProvider} from 'contexts/currentUser'
+// import CurrentUserChecker from 'components/currentUserChecker'
 
 const App = () => {
   return (
-    <Router>
-      <TopBar/>
-      <Routes/>
-    </Router>
+    <CurrentUserProvider>
+      <Router>
+        <TopBar/>
+        <Routes/>
+      </Router>
+    </CurrentUserProvider>
   )
 }
 
